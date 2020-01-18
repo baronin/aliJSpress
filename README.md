76dc895c2265aa8ad3df8d445ad26740ad1d474e
Yaroslav <baronin.yaroslav@gmail.com>
2020-01-19 00:05:13 +0200

    added function sort

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# alijsecpres
- javascript
+#Start project
+`node v 10.17.0`
+`npm i` <br>
+`npm run dev` 
--- js/index.js
+++ js/index.js
@@ -1,63 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const searchClass = document.querySelector('.search');
-    const cartBtnId = document.getElementById('cart');
-    const wishListBtnId = document.getElementById('wishlist');
-    const cartClass = document.querySelector('.cart');
-    const closeCart = document.querySelector('.cart-close');
-    const goodWrapper = document.querySelector('.goods-wrapper');
+  const searchClass = document.querySelector('.search');
+  const cartBtnId = document.getElementById('cart');
+  const wishListBtnId = document.getElementById('wishlist');
+  const cartClass = document.querySelector('.cart');
+  const closeCart = document.querySelector('.cart-close');
+  const goodWrapper = document.querySelector('.goods-wrapper');
+  const categoryClass = document.querySelector('.category');
 
-    goodWrapper.appendChild(createCardGoods())
+  const urlAPI = 'db/db.json';
+  const getGoodsAPI = (handler, filter) => {
+    loading();
+    fetch(urlAPI)
+        .then(response => response.json())
+        .then(filter)
+        .then(handler);
+  };
+  goodWrapper.textContent = '';
+  const randomSort = goods => goods.sort(() => Math.random() - 0.5);
 
-    const openCart = () => {
-        cartClass.style.display = 'block';
-    };
-    const isCloseCart = (event) => {
-        const target = event.target;
-        if (target === closeCart || target.classList.contains('cart-close')) {
-            cartClass.style.display = "none";
-        }
-    };
-    const isCloseEsc = cartBtnId.onkeydown = function (evt) {
-        evt = evt || window.event;
-        var isEscape = false;
-        if ("key" in evt) {
-            isEscape = (evt.key === "Escape" || evt.key === "Esc");
-        } else {
-            isEscape = (evt.keyCode === 27);
-        }
-        if (isEscape) {
-            cartClass.style.display = "none";
-        }
-    };
+  const renderCart = goods => {
+    goods.forEach((item) => {
+      const { id, title, price, imgMin } = item;
+      goodWrapper.appendChild(
+          createCardGoods(id, title, price, `../${ imgMin }`));
+    });
+  };
 
-    cartBtnId.addEventListener('click', openCart);
-    closeCart.addEventListener('click', isCloseCart);
+  const chooseCategories = event => {
+    event.preventDefault();
+    goodWrapper.textContent = '';
+    const target = event.target;
 
-    isCloseEsc();
-});
+    if (target.classList.contains('category-item')) {
+      const cat = target.dataset.category;
+      getGoodsAPI(renderCart, goods => goods
+          .filter(item => item.category.includes(cat)));
+    }
+  };
 
 
-const createCardGoods = (id, title, price, img) => {
+  const openCart = event => {
+    event.preventDefault();
+    cartClass.style.display = 'block';
+    document.addEventListener('keyup', isCloseCart);
+  };
+  const isCloseCart = event => {
+    event.preventDefault();
+    const target = event.target;
+    if (target === cartBtnId
+        || target.classList.contains('cart-close')
+        || event.key === 2) {
+      cartClass.style.display = 'none';
+      document.removeEventListener('keyup', isCloseCart);
+    }
+  };
+
+
+  cartBtnId.addEventListener('click', openCart);
+  closeCart.addEventListener('click', isCloseCart);
+  categoryClass.addEventListener('click', chooseCategories);
+
+  getGoodsAPI(renderCart, randomSort);
+  const createCardGoods = (id, title, price, img) => {
     const card = document.createElement('div');
     card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
     card.innerHTML = `
                     <div class="card">
                         <div class="card-img-wrapper">
-                            <img class="card-img-top" src="img/${img}" alt="">
+                            <img class="card-img-top" src="img/${ img }" alt="">
                             <button
                             class="card-add-wishlist"
-                            data-goods-id-${id}
+                            data-goods-id-${ id }
                             ></button>
                         </div>
                         <div class="card-body justify-content-between">
-                            <a href="#" class="card-title">${title}</a>
-                            <div class="card-price">${price} ₽</div>
+                            <a href="#" class="card-title">${ title }</a>
+                            <div class="card-price">${ price } ₽</div>
                             <div>
                                 <button class="card-add-cart">Добавить в корзину</button>
                             </div>
                         </div>
                     </div>
-                    `
+                    `;
     return card;
-}
+  };
+
+});
+
